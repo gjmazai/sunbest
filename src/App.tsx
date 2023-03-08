62396dcaa625a3b47daefa57efb376c63fef6a1c
Shilov Stas <sofabb9@gmail.com>
2023-03-08 21:28:09 +0700

    add useEffect for fetch promotion products

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./assets/scss/App.scss";
 import Preview from "./pages/Preview";
 import Main from "./pages/Main";
 
+import { useActions } from "./hooks/useAction";
+
 function App() {
+	const { fetchPromotionProducts } = useActions();
+	useEffect(() => {
+		fetchPromotionProducts();
+	}, []);
+
 	return (
 		<div className="App">
 			<Preview />
