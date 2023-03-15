7393272d7bd4676b74a4efa389b899487c5e4967
Shilov Stas <sofabb9@gmail.com>
2023-03-16 00:12:08 +0700

    new component

---
+++ src/pages/PreviewProducts.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import PromotionsProductsList from "../components/PromotionsProductsList";
+
+const PreviewProducts = () => {
+	return (
+		<div>
+			<PromotionsProductsList />
+		</div>
+	);
+};
+
+export default PreviewProducts;
