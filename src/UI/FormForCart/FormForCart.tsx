a76d444e23853485992319b9b5bfa1befbeb14c4
Shilov Stas <sofabb9@gmail.com>
2023-03-17 22:41:57 +0700

    create new component

---
+++ src/UI/FormForCart/FormForCart.tsx
@@ -0,0 +1,43 @@
+import React, { FC } from "react";
+import { IPromotionProducts } from "../../models/IPromotionProducts";
+import { costDenominator } from "../../utils/costDenominator";
+
+import classes from "./ForamForCart.module.scss";
+
+interface FormForCartProps {
+	product: IPromotionProducts;
+	setProduct: (newProduct: IPromotionProducts) => void;
+}
+
+const FormForCart: FC<FormForCartProps> = ({ product, setProduct }) => {
+	return (
+		<div className={classes.form}>
+			<h2 className="PromotionProduct__cost">{costDenominator(product.cost)} ₽</h2>
+			<h5 className="PromotionProduct__fullName">{product.fullName}</h5>
+			<ul className="PromotionProduct__list">
+				<li>
+					Размеры: <span>{product.size}</span>
+				</li>
+				<li>
+					Бренд: <span>{product.brand}</span>
+				</li>
+				<li>
+					Страна: <span>{product.country}</span>
+				</li>
+				<li>
+					Код: <span>{product.id}</span>
+				</li>
+			</ul>
+			<button
+				className={classes.button}
+				onClick={() => {
+					setProduct(product);
+				}}
+			>
+				Добавить товар в корзину
+			</button>
+		</div>
+	);
+};
+
+export default FormForCart;
