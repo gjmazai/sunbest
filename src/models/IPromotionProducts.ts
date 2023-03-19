92674f2dcb94cc5138593a2b422cbd23b49120b7
Shilov Stas <sofabb9@gmail.com>
2023-03-19 20:37:52 +0700

    new filed in IPromotionProducts interface

--- src/models/IPromotionProducts.ts
+++ src/models/IPromotionProducts.ts
@@ -1,11 +1,13 @@
 export interface IPromotionProducts{
 	fullName: string;
+	category: string,
 	cost: number;
 	size: string;
 	brand: string;
 	country: string;
 	id: number;
-	promotion?: boolean;
-	availability?: boolean;
+	promotion: boolean;
+	availability: boolean;
+	promoCost: number | null,
 	img: string;
 }
